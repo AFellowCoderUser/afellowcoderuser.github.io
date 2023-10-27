e81d237803a58940257169f97382b7860e32975e
AFellowCoderUser <109687165+AFellowCoderUser@users.noreply.github.com>
2023-10-26 20:08:38 -0400

    Major changes

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,6 +1,6 @@
 var html = '<a href="https://afellowcoderuser.github.io/">Home</a>';
 
 html += '<a href="https://afellowcoderuser.github.io/learnhtml.html">HTML</a>';
-html += '<a href="">CSS</a>';
+html += '<a href="https://afellowcoderuser.github.io/learncss.html">CSS</a>';
 
 document.getElementById("navbar").innerHTML = html;
