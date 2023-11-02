49918b1de62289c050674e501c4f5c08edccf13e
AFellowCoderUser <gotimemel@outlook.com>
2023-11-02 08:46:28 -0400

    page separation + navbar update

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,5 +1,6 @@
 var html = '<a href="https://afellowcoderuser.github.io/">Home</a>';
 
+html += '<a href="https://afellowcoderuser.github.io/start.html">Intro</a>';
 html += '<a href="https://afellowcoderuser.github.io/learnhtml.html">HTML</a>';
 html += '<a href="https://afellowcoderuser.github.io/learncss.html">CSS</a>';
 
